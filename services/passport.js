8e679f74ba3cc3feb2d925a8bb1a1611441855bd
greksn <gregor.mercell@gmail.com>
2019-01-08 18:05:52 +0700

    finished auth flow

--- services/passport.js
+++ services/passport.js
@@ -1,10 +1,31 @@
 const keys = require('../config/keys');
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStategy = require('passport-google-oauth20').Strategy;
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(new GoogleStategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refresfToken, profile, done) => {
+  User.findOne({googleId: profile.id}).then((existingUser)=> {
+    if(existingUser){
+      done(null, existingUser);
+    } else{
+      new User({googleId: profile.id, name: profile.name.givenName}).save().then((user) => done(null, user));
+    }
+  })
   console.log("profile: ", profile);
 }));
